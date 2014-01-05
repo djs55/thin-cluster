a3ac442d51aa51ef07f28fbd307509ed59898154
David Scott <dave.scott@eu.citrix.com>
2014-01-05 22:34:04 +0000

    Incremental commit
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- lib/allocator.mli
+++ lib/allocator.mli
@@ -11,4 +11,25 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *)
-include module type of Lvm.Allocator
+
+module Interval = struct
+  type t = int64 * int64
+
+  let create start length = start, length
+
+  let difference (a_start, a_length) (b_start, b_length) =
+    failwith "unimplemented"
+
+  let union (a_start, a_length) (b_start, b_length) =
+    failwith "unimplemented"
+
+  let is_empty (_, x) = x = 0L
+end
+
+type t = Interval.t list
+
+let create start length = [ Interval.create start length ]
+
+let difference a b =
+  let one b = List.fold_left Interval.difference b a in
+  List.fold_left one 
